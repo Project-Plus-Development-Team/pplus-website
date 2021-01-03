805c8000737e5a2e054c17e95d201d3f978c5dda
waffeln <sinusstudios@gmail.com>
2021-01-03 16:46:57 +0100

    nice, smart redirects

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+const path = require("path");
+const redirectParser = require("netlify-redirect-parser");
+
 module.exports = {
   webpack: config => {
     config.module.rules.push({
@@ -6,5 +10,17 @@ module.exports = {
     });
     
     return config;
+  },
+
+  // this only affects dev environment and reads the redirects from the netlify.toml file to replicate behaviour in dev env
+  async redirects() {
+    const tomlFilePath = path.join(process.cwd(), "netlify.toml");
+    const { success } = await redirectParser.parseNetlifyConfig(tomlFilePath);
+
+    return success.map(({ path: source, to: destination }) => ({
+      source,
+      destination,
+      permanent: true
+    }));
   }
 };

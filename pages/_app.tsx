0ae46c5049116be3c57d31650fd65df750cca580
waffeln <sinusstudios@gmail.com>
2021-01-03 23:22:30 +0100

    update favicon and meta stuff

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,16 +13,23 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
-        <meta name="keywords" content="Smash, Project+, Project M, PM, P+, Smash Mod, Brawl Mod, Update, ProjectM, Project M, P+, Project+, Project Plus, Smash Bros Project Plus, Project M Plus, PM, Smash Bros Brawl, Brawl Mods, Smash Bros Brawl Mods, project m, project m 3.6, project melee, project melee plus, project melee plus, project m mod, project m game, project m plus, jank, knuckles"/>
-        <meta name="description" content="Project+ is a balance patch for Project M, a popular Super Smash Bros. Brawl mod"/>
-        <meta name="author" content="waffeln"/>
+        <link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon.png?v2=dLXjO2Kp56"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v2=dLXjO2Kp56"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v2=dLXjO2Kp56"/>
+        <link rel="manifest" href="/site.webmanifest?v2=dLXjO2Kp56"/>
+        <link rel="mask-icon" href="/safari-pinned-tab.svg?v2=dLXjO2Kp56" color="#34d171"/>
+        <link rel="shortcut icon" href="/favicon.ico?v2=dLXjO2Kp56"/>
+        <meta name="msapplication-TileColor" content="#34d171"/>
 
         {/* theme color for mobile phone tabs */}
         <meta name="theme-color" content="#34d171"/>
 
         {/* preview image for embeds in some services */}
-        <meta name="og:image" content="/img/favicon.png"/>
+        <meta name="og:image" content="/images/favicon.png"/>
+
+        <meta name="keywords" content="Smash, Project+, Project M, PM, P+, Smash Mod, Brawl Mod, Update, ProjectM, Project M, P+, Project+, Project Plus, Smash Bros Project Plus, Project M Plus, PM, Smash Bros Brawl, Brawl Mods, Smash Bros Brawl Mods, project m, project m 3.6, project melee, project melee plus, project melee plus, project m mod, project m game, project m plus, jank, knuckles"/>
+        <meta name="description" content="Project+ is a balance patch for Project M, a popular Super Smash Bros. Brawl mod"/>
+        <meta name="author" content="waffeln"/>
         
         <title>Project+ Website</title>
       </Head>

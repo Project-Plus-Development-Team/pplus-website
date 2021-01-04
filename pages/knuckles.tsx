7a852584d8fd6fb98ea54650b5a4f3e09ce9bfb1
waffeln <sinusstudios@gmail.com>
2021-01-04 20:49:02 +0100

    fix knuckles page text duplicate

--- pages/knuckles.tsx
+++ pages/knuckles.tsx
@@ -40,7 +40,7 @@ export default function Knuckles() {
               Knuckles digs out of development hell and into Project+! The iconic red echidna has been polished and refined to fit in nicely with the existing cast. While technically based on Sonic, Knuckles’s moveset is almost entirely unique, with different normals, specials, and character properties to make him feel completely different than his blue, spiky rival.
             </InfoElement>
             <InfoElement title="rougher than the rest of 'em" image="knuckles3.png" position="right">
-              Knuckles digs out of development hell and into Project+! The iconic red echidna has been polished and refined to fit in nicely with the existing cast. While technically based on Sonic, Knuckles’s moveset is almost entirely unique, with different normals, specials, and character properties to make him feel completely different than his blue, spiky rival.
+            Originally designed as a stronger version of Sonic, Knuckles’s moveset obviously has some heavy hitters. His forward aerial is similar to Mario’s, having both a strong outward sending hitbox as well as a meteor on the descent. His down and up tilts are both incredible combo starters that lead into his fast and powerful aerials. Up Smash is a destructive punch upwards that can shark platforms and set opponents ablaze. Knuckles retains Spin Dash, but adds in different properties to make his version unique.
             </InfoElement>
           </Columns.Column>
         </Columns>

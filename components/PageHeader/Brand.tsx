7e213c1b8fbb331a0bdd4f0198fe5518078b9930
FunctionDJ <34871211+FunctionDJ@users.noreply.github.com>
2021-01-18 17:34:24 +0100

    spiiiin

--- components/PageHeader/Brand.tsx
+++ components/PageHeader/Brand.tsx
@@ -14,19 +14,25 @@ export default function Brand({ isNavbarActive, setNavbarActive, isHome }: Brand
 
   const [eggCounter, setEggCounter] = React.useState(1);
   const [spin, setSpin] = React.useState(false);
+  const [useAltSpin, setUseAltSpin] = React.useState(false);
 
   function egg() {
     if (!isHome) {
       return;
     }
 
-    if (eggCounter >= 7) {
+    if (eggCounter >= 15) {
       setEggCounter(0);
       setSpin(true);
 
+      const spinAudio = new Audio(`/spin${useAltSpin ? 2 : 1}.mp3`);
+      spinAudio.volume = 0.3;
+      spinAudio.play();
+
       setTimeout(() => {
         setSpin(false);
-      }, 500);
+        setUseAltSpin(!useAltSpin);
+      }, 3000);
 
       return;
     }
@@ -44,7 +50,7 @@ export default function Brand({ isNavbarActive, setNavbarActive, isHome }: Brand
             style={{ lineHeight: 1 }}
             title={isHome ? "You're already on the home page" : ""}
             onClick={egg}
-            className={"logo " + (spin ? "spin" : "")}
+            className={"logo " + (spin ? `spin${useAltSpin ? 2 : 1}` : "")}
           >
             <SmartImage
               img={require("../../public/images/logo.png?size=178")}
@@ -52,9 +58,14 @@ export default function Brand({ isNavbarActive, setNavbarActive, isHome }: Brand
             />
           
             <style jsx global>{`            
-              .logo.spin {
-                transition: transform 0.5s ease-in-out;
-                transform: rotateZ(360deg);
+              .logo.spin1 {
+                transition: transform 3s ease-out;
+                transform: rotateZ(6turn);
+              }
+              
+              .logo.spin2 {
+                transition: transform 3s ease-out;
+                transform: rotateZ(12turn);
               }
             `}</style>
           </a>

6c5128d14feaa5479ea8179a4a8846e47f4c1af2
Motobug <ssbmotobug@gmail.com>
2021-01-25 23:25:57 -0500

    Revert "Make anchor scrolling align the target to the top (better position)"
    This reverts commit 757a80d74c4721283d82345422b379e4e24f27f4.

--- components/Changes/Character.tsx
+++ components/Changes/Character.tsx
@@ -22,7 +22,7 @@ export default function Character({ data: { name, moves }, version, siteUrl, fol
     if (!fold) {
       setShow(true);
 
-      parentElement.current.scrollIntoView(true, {
+      parentElement.current.scrollIntoView({
         behavior: "smooth"
       });
     }

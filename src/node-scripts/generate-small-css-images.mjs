aaac60e6f85f15a57e4b9f96795ffab736cff8e2
Function <34871211+FunctionDJ@users.noreply.github.com>
2022-04-10 22:15:58 +0200

    improve build speed by removing await on convert images

--- src/node-scripts/generate-small-css-images.mjs
+++ src/node-scripts/generate-small-css-images.mjs
@@ -1,36 +1,36 @@
 import { convertImages, convertSingleImage } from "../node-utilities/generate-lib.mjs";
 
-await convertImages("characters", { resize: { height: 40 }});
+convertImages("characters", { resize: { height: 40 }});
 
-await convertImages("communities", {
+convertImages("communities", {
   quality: 50,
   resize: { height: 48 }
 });
 
-await convertImages("css", {
+convertImages("css", {
   flop: true,
   quality: 50,
   resize: { width: 80, height: 80 }
 });
 
-await convertImages("q9283rw8fg/css", {
+convertImages("q9283rw8fg/css", {
   flop: true,
   quality: 50,
   resize: { width: 80, height: 80 }
 });
 
-await convertImages("icons", { resize: { height: 40 }});
-await convertImages("knuckles", { quality: 100, resize: { width: 300 } });
+convertImages("icons", { resize: { height: 40 }});
+convertImages("knuckles", { quality: 100, resize: { width: 300 } });
 
-await convertSingleImage("background.jpeg");
-await convertSingleImage("background-banner.png");
-await convertSingleImage("falco-glowy.png", { resize: { height: 366 }});
-await convertSingleImage("logo.png");
-await convertSingleImage("mosaic-background-pattern.jpg", { quality: 100 });
-await convertSingleImage("smashgg-favicon.png");
-await convertSingleImage("favicon.png");
-await convertSingleImage("knuckles-woah.png", { resize: { height: 300 }, quality: 50 });
+convertSingleImage("background.jpeg");
+convertSingleImage("background-banner.png");
+convertSingleImage("falco-glowy.png", { resize: { height: 366 }});
+convertSingleImage("logo.png");
+convertSingleImage("mosaic-background-pattern.jpg", { quality: 100 });
+convertSingleImage("smashgg-favicon.png");
+convertSingleImage("favicon.png");
+convertSingleImage("knuckles-woah.png", { resize: { height: 300 }, quality: 50 });
 
-await convertSingleImage("q9283rw8fg/project-wave-logo.png", { quality: 90 });
-await convertSingleImage("q9283rw8fg/vaporwave-color-palette.png", { resize: { height: 20 }, quality: 30 });
-await convertSingleImage("q9283rw8fg/vaporwave-wallpaper.jpg", { resize: { height: 1000 }, quality: 80 });
+convertSingleImage("q9283rw8fg/project-wave-logo.png", { quality: 90 });
+convertSingleImage("q9283rw8fg/vaporwave-color-palette.png", { resize: { height: 20 }, quality: 30 });
+convertSingleImage("q9283rw8fg/vaporwave-wallpaper.jpg", { resize: { height: 1000 }, quality: 80 });

be6b9eb3938b15b07a2a7484564eb6f110d8064d
FunctionDJ <34871211+FunctionDJ@users.noreply.github.com>
2025-06-29 18:08:37 +0200

    Remove obsolete logic in changes page

--- app/changes/[version]/page.tsx
+++ app/changes/[version]/page.tsx
@@ -4,61 +4,55 @@ import { SingleVersionChanges } from "../components/SingleVersionChanges";
 import { getSingleVersionData, getSortedVersions } from "../get-version";
 
 export async function generateStaticParams() {
-	const versions = ["all", ...(await getSortedVersions())];
+  const versions = ["all", ...(await getSortedVersions())];
 
-	return versions.map((version) => ({
-		version,
-	}));
+  return versions.map((version) => ({
+    version,
+  }));
 }
 
 type Props = {
-	params: Promise<{
-		version: string;
-	}>;
+  params: Promise<{
+    version: string;
+  }>;
 };
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
-	const { version } = await params;
+  const { version } = await params;
 
-	if (version === "all") {
-		return {
-			title: "Changes from all versions",
-			description:
-				"View all changes from all Project+ versions automatically combined.",
-		};
-	}
+  if (version === "all") {
+    return {
+      title: "Changes from all versions",
+      description:
+        "View all changes from all Project+ versions automatically combined.",
+    };
+  }
 
-	return {
+  return {
     title: `Changes in v${version}`,
     description: `View everything that's been changed in Project+ v${version}`,
-	};
+  };
 }
 
 export default async function VersionChangesShared({ params }: Props) {
-	const { version } = await params;
+  const { version } = await params;
 
-	if (version === undefined || Array.isArray(version)) {
-		throw new Error(
-			"/pages/changes/[version] param unexpectedly undefined"
-		);
-	}
+  if (version === "all") {
+    const versions = await getSortedVersions();
+    const data = await Promise.all(versions.map(getSingleVersionData));
 
-	if (version === "all") {
-		const versions = await getSortedVersions();
-		const data = await Promise.all(versions.map(getSingleVersionData));
+    return (
+      <main data-q="changes">
+        <AllVersionChanges data={data} />
+      </main>
+    );
+  }
 
-		return (
-			<main data-q="changes">
-				<AllVersionChanges data={data} />
-			</main>
-		);
-	}
+  const data = await getSingleVersionData(version);
 
-	const data = await getSingleVersionData(version);
-
-	return (
-		<main data-q="changes">
-			<SingleVersionChanges data={data} />
-		</main>
-	);
+  return (
+    <main data-q="changes">
+      <SingleVersionChanges data={data} />
+    </main>
+  );
 }
